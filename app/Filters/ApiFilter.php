234538089304992349ea530c8720a5b32c718cb9
Philip Newborough <corenominal@corenominal.org>
2026-03-11 08:52:45 +0000

    Update api filter to only work with master api key

--- app/Filters/ApiFilter.php
+++ app/Filters/ApiFilter.php
@@ -56,38 +56,10 @@ class ApiFilter implements FilterInterface
         // Set success flag
         $success = false;
         
-        // Test against 'apikeys.masterKey' .env values first, then database
-        // Load API keys configuration
+        // Test against 'apikeys.masterKey' .env value
         $config = config('ApiKeys');
         if ($config->masterKey == $apikey) {
             $success = true;
-        }
-
-        if(!$success){
-            // Reset success flag
-            $success = true;
-            // Test user UUID is provided
-            if (!$request->hasHeader('user-uuid')) {
-                header('HTTP/1.1 401 Unauthorized', true, 401);
-                exit(json_encode(['error' => 'No user UUID provided.']));
-            }
-            // Get the user UUID from the header
-            $user_uuid = $request->header('user-uuid')->getValue();
-            // cURL GET request to auth server
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, config('Urls')->auth . 'api/keycheck/' . $user_uuid . '/' . $apikey);
-            // Set 'apikey' header
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array('apikey: ' . config('ApiKeys')->masterKey));
-            // Return the transfer as a string
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            $response = curl_exec($ch);
-            $response = json_decode($response);
-
-            // Test for error response
-            if(isset($response->error)){
-                // Error response, set flag to false
-                $success = false;
-            }
         }
 
         // Test flag
